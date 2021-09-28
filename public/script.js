72f13094e05193b9754fe9c32c5cc812a1630e32
Sachin Chouksey <sacchouk@publicisgroupe.net>
2021-09-28 20:39:57 +0530

    Added with reciver video

--- public/script.js
+++ public/script.js
@@ -3,15 +3,11 @@ const myVideo = document.createElement('video');
 const videoGrid = document.getElementById('video-grid')
 myVideo.muted = true;
 console.log("<%= roomId %>");
-var peer = new Peer(undefined, {
-    path: '/peerjs',
-    host: '/',
-    port: '3000'
-});
+var peer = new Peer();
 var socket = io();
 
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+    navigator.mediaDevices.getUserMedia({ video: false, audio: false }).then(stream => {
         console.log(stream);
         myVideoStream = stream;
         addVideoStream(myVideo, myVideoStream)
@@ -33,6 +29,7 @@ const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
         video.play()
+        video.muted=true
         videoGrid.append(video)
 
     })
@@ -47,6 +44,7 @@ peer.on('call', call=> {
     call.answer(myVideoStream); // Answer the call with an A/V stream.
     const video = document.createElement('video');
     video.muted=true
+    // stream comming from caller 
         call.on('stream', userVideoStream => {
             console.log("remopte str");
             addVideoStream(video, userVideoStream)
@@ -57,7 +55,9 @@ const connectToNewUser = (userId, stream) => {
     console.log("New User is connected-->", userId, stream);
     const call = peer.call(userId, stream)
     const video = document.createElement('video');
+    // stream comming from person which we have call
     call.on('stream', function(remoteStream) {
+        console.log("remote stream");
         // Show stream in some video/canvas element.
         addVideoStream(video, remoteStream)
 
